e72b76d34eea9db70890489a4e844e58c2e2d040
bluuweb <contacto@bluuweb.cl>
2022-09-01 23:18:44 -0400

    cors public

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import axios from "axios";
 import * as cheerio from "cheerio";
+import cors from "cors";
 const app = express();
 
-app.get("/", async (req, res) => {
+app.get("/", cors(), async (req, res) => {
     console.log("entró aquí");
     try {
         const { data } = await axios.get("https://www.bcentral.cl/inicio");

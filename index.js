8f7c0d820117a72defae2263c1b5a21908524a32
bluuweb <contacto@bluuweb.cl>
2022-09-01 22:50:44 -0400

    version alpha

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import axios from "axios";
+import * as cheerio from "cheerio";
+const app = express();
+
+app.get("/", async (req, res) => {
+    try {
+        const { data } = await axios.get("https://www.bcentral.cl/inicio");
+
+        const $ = cheerio.load(data);
+        const selectorDolar =
+            "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(3) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2";
+
+        const selectorUF =
+            "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(1) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2";
+
+        const objetoValores = {
+            // fecha: new Date().toLocaleString("es-ES"),
+            fecha: new Date().toLocaleDateString(),
+            UF: $(selectorUF).text() ?? "sin datos",
+            // dolar: $(selectorDolar).html().split(" ")[1].trim(),
+            dolar: $(selectorDolar).text().split("/")[0].trim() ?? "sin datos",
+        };
+
+        res.json(objetoValores);
+    } catch (error) {
+        res.json({ error });
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("server en ⭐ Puerto:" + PORT));

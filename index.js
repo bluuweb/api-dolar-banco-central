00482921967e7a0882de7f52d64c29cdedf48b03
bluuweb <contacto@bluuweb.cl>
2022-09-10 16:14:47 -0400

    url not found

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(cors());
 
 app.use("/api/v1/", coinRouter);
+app.use("/*", (_, res) =>
+    res.json({
+        msg: "API pública creada con amor, por favor utilizar con moderación.",
+        version: "1.0.0",
+        discord: "https://discord.com/invite/hcG7NBvsGD",
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("server en ⭐ Puerto:" + PORT));

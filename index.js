b207c6edda713152b13a887131482656798924e5
Vicente Alcañiz Huerta <vialhue@gmail.com>
2022-09-10 17:39:49 +0200

    Mejora para poder scrapear N elementos y devolver solo los que tiene valor.

--- index.js
+++ index.js
@@ -7,60 +7,102 @@ const app = express();
 app.get("/", cors(), async (req, res) => {
     console.log("entró aquí cors");
     try {
-        // const { data } = await axios.get("https://www.bcentral.cl/inicio");
-        // const dataBitcoin = await axios.get("https://es.investing.com/crypto/bitcoin/btc-usd")
+        // La idea la voy a ir describiendo por pasos, y al final veremos las conclusiones:
 
-        const [dataBC, dataBitcoin] = await Promise.allSettled([
-            axios.get("https://www.bcentral.cl/inicio"),
-            axios.get("https://coinmarketcap.com/es/currencies/bitcoin/"),
-        ]);
+        // 1 - Crear la lista de "scrapeos" por llamarlos de alguna forma, esto podría crearse en otro método, en otro fichero, tal vez que lo recuperara de una
+        //     base de datos de scrapeos o algo así.
 
-        // if (dataBC.status === "rejected") throw ({ msg: dataBC.reason });
+        const scrapers = [
+            {
+                title: "dollar",
+                promiseUrl : "https://www.bcentral.cl/inicio",
+                axiospromise: axios.get("https://www.bcentral.cl/inicio"),
+                selector: "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(3) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2"
+            },
+            {
+                title: "UF",
+                promiseUrl : "https://www.bcentral.cl/inicio",
+                axiospromise: axios.get("https://www.bcentral.cl/inicio"),
+                selector: "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(1) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2"
+            },
+            {
+                title: "euro",
+                promiseUrl : "https://www.bcentral.cl/inicio",
+                axiospromise: axios.get("https://www.bcentral.cl/inicio"),
+                selector: "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(4) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2"
+            },
+            {
+                title: "bitcoin",
+                promiseUrl : "https://coinmarketcap.com/es/currencies/bitcoin/",
+                axiospromise: axios.get("https://coinmarketcap.com/es/currencies/bitcoin/"),
+                selector: "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.eMxKgr.container > div.n78udj-0.jskEGI > div > div.sc-16r8icm-0.kjciSH.priceSection > div.sc-16r8icm-0.kjciSH.priceTitle > div > span"
+            },
+        ];
 
-        let $ = cheerio.load(dataBC.value.data);
-        const selectorDolar =
-            "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(3) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2";
+        // console.log(scrapers);
 
-        const selectorUF =
-            "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(1) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2";
+        // 2 - Recuperar el array de promesas para poder lanzarlas todas juntas, en principio doy por supuesto que habrá tantos resultados como promesas, tanto
+        //     si fallar como van bien.
 
-        const selectorEuro =
-            "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(4) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2";
+        const promisesList = scrapers.map( (item) => { 
+            return item.axiospromise;
+        } );
 
-        const objetoDolar = {
-            text: "dolar",
-            valor: $(selectorDolar).text().split("/")[0].trim() ?? "sin datos",
-            fecha: new Date().toLocaleDateString(),
-        };
+        // console.log("promisesList: " + promisesList);
 
-        const objetoUF = {
-            text: "UF",
-            valor: $(selectorUF).text() ?? "sin datos",
-            fecha: new Date().toLocaleDateString(),
-        };
+        // 3 - Lanzar todas las promesas juntas con el Promise.allSettled, entiendo que el proceso terminará con la petición que más tarde, habiendo completado todas.
 
-        const objetoEuro = {
-            text: "Euro",
-            valor: $(selectorEuro).text() ?? "sin datos",
-            fecha: new Date().toLocaleDateString(),
-        };
+        const resultPromises = await Promise.allSettled(promisesList);
 
-        // bitcoin
+        // console.log("resultPromises " + resultPromises)
 
-        $ = cheerio.load(dataBitcoin.value.data);
-        const selectorBitcoin =
-            "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.eMxKgr.container > div.n78udj-0.jskEGI > div > div.sc-16r8icm-0.kjciSH.priceSection > div.sc-16r8icm-0.kjciSH.priceTitle > div > span";
+        // 4 - Recorreremos los resultados de las promesas, la premisa es que debe haber tantos resultados como "scrapers", que son los mismos que las promesas que se han
+        //     lanzado, y además (esto es muy importante) deben estar en el mismo orden en que se han lanzado.
+        //     Para cada resultado, haremos el scrapping con cheerio y lo montamos en un objeto que es lo que retornaremos.
 
-        // console.log(dataBitcoin.value);
+        const resultScraping = resultPromises.map( (result, index) => {
+            // console.log("result.status : " + result.status)
+            if (result.status == "fulfilled") {
+                const $ = cheerio.load(result.value.data);
+                const scrappValue = $(scrapers[index].selector).text().split("/")[0].trim();
+                return {
+                    text: scrapers[index].title,
+                    valor: scrappValue,
+                    fecha: new Date().toLocaleDateString()
+                }
+            } else {
+                return {
+                    text: scrapers[index].title,
+                    valor: "",
+                    fecha: new Date().toLocaleDateString()
+                }
+            }
+        })
 
-        const objetoBitcoin = {
-            text: "Bitcoin",
-            valor: $(selectorBitcoin).text() ?? "sin datos",
-            fecha: new Date().toLocaleDateString(),
-        };
+        // console.log("resultScraping : " + resultScraping)
 
-        return res.json([objetoDolar, objetoUF, objetoEuro, objetoBitcoin]);
+        // 5 - Filtramos los resultados para solo devolver los que tienen "valor". (Esto es, según se mire, opcional, ya que el cliente se podría hacer "algo" para
+        //     cuando un "text" tiene un valor inconsistente. Tal vez poner un icono "interrogante" o no se)
+
+        const filteredResult = resultScraping.filter(item => item.valor != "")
+
+        // console.log("filteredResult : " + filteredResult)
+
+        return res.json(filteredResult);
+
+        // Conclusiones:
+        // - Para añadir un nuevo scrapeo, solo hay que añadir un nuevo elemento a los "scrapers"
+        // - Tener en cuenta que el valor scrapeado va a tener que tener en cuenta los splits, trim o replaces que haya que hacer. Ahora mismo esto es IGUAL para todos los
+        //   valores scrapeados (mirar "resultPromises.map" para comprender lo que quiero decir). Se podría llevar a un método a parte que recibiera el "result" y el 
+        //   "scrapers[i]" para tratarlo de una forma especial, aunque en mi opinión diría que todos los scrapeos van a mantener la misma operación sobre "text()", tal 
+        //   vez algún split o replace pero que le va a venir bien a cualquier valor que se scrapee. No obstante es una desventaja.
+        // - He filtrado los resultados con valor vacio ... pero podría no filtrarse y que en cliente se hiciera algo, no se, pintar el ROJO el "text" o poner un 
+        //   icono de "?" ... ideas varias.
+        // - He puesto console.log por todos los pasos, si los activais podreis ir viendo los resultados en caso de que no comprendais algún paso (yo creo que no
+        //   he hecho nada especialmente raro, pero por si acaso.)
+
     } catch (error) {
+        console.log("Error : " + error)
         res.json({ error });
     }
 });
